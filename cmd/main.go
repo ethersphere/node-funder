3dd4d1d9a2aea7fe7c7259336cb985d64b0046bb
Vlado Pajic <vlado@brrrrr.at>
2022-11-15 16:19:04 +0100

    add header

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,8 @@
-package cmd
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
 
 import "fmt"
 
