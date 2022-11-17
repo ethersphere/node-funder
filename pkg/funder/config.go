948423ddafd1cd1819924b8972bbfd696361e079
Vlado Pajic <vlado@brrrrr.at>
2022-11-17 18:03:02 +0100

    update token names

--- pkg/funder/config.go
+++ pkg/funder/config.go
@@ -10,19 +10,17 @@ import (
 )
 
 type Config struct {
-	Namespace string
-	MinETH    float64
-	MinBZZ    float64
-	MinGBZZ   float64
+	Namespace     string
+	MinNativeCoin float64 // on mainnet this is ETH
+	MinBZZToken   float64 // on mainnet this is BZZ
 }
 
 func ParseConfig() (Config, error) {
 	cfg := Config{}
 
 	flag.StringVar(&cfg.Namespace, "namespace", "", "kuberneties namespace")
-	flag.Float64Var(&cfg.MinETH, "minETH", 0, "specifies min amout of ETH tokens nodes should have")
-	flag.Float64Var(&cfg.MinBZZ, "minBZZ", 0, "specifies min amout of BZZ tokens nodes should have")
-	flag.Float64Var(&cfg.MinGBZZ, "minGBZZ", 0, "specifies min amout of GBZZ tokens nodes should have")
+	flag.Float64Var(&cfg.MinNativeCoin, "minNativ", 0, "specifies min amout of ETH tokens nodes should have")
+	flag.Float64Var(&cfg.MinBZZToken, "minBzz", 0, "specifies min amout of BZZ tokens nodes should have")
 	flag.Parse()
 
 	if cfg.Namespace == "" {
