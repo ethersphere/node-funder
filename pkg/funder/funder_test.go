a558553c8f8144b43b13f49af302dbca1ef7ce5b
vladopajic <vladopajic@users.noreply.github.com>
2023-05-05 14:25:07 +0200

    test coverage improvements (#25)

--- pkg/funder/funder_test.go
+++ pkg/funder/funder_test.go
@@ -7,6 +7,8 @@ package funder_test
 import (
 	"context"
 	"math/big"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -36,9 +38,27 @@ func Test_Fund(t *testing.T) {
 	t.Run("fund addresses - set", func(t *testing.T) {
 		t.Parallel()
 
-		cfg := Config{Addresses: []string{"0x95f8916183f7C7154e49396507F5b0FafA4d8077"}}
-		err := Fund(ctx, cfg, nil, w)
-		assert.Error(t, err)
+		t.Run("already funded (0,0)", func(t *testing.T) {
+			t.Parallel()
+
+			cfg := Config{Addresses: []string{"0x95f8916183f7C7154e49396507F5b0FafA4d8077"}}
+			err := Fund(ctx, cfg, nil, w)
+			assert.NoError(t, err)
+		})
+
+		t.Run("not funded (3,3)", func(t *testing.T) {
+			cfg := Config{
+				Addresses: []string{
+					"0x95f8916183f7C7154e49396507F5b0FafA4d8077",
+					"0x95f8916183f7C7154e49396507F5b0FafA4d8071",
+					"0x95f8916183f7C7154e49396507F5b0FafA4d8072",
+					"0x95f8916183f7C7154e49396507F5b0FafA4d8073",
+				},
+				MinAmounts: MinAmounts{NativeCoin: 3, SwarmToken: 3},
+			}
+			err := Fund(ctx, cfg, nil, w)
+			assert.NoError(t, err)
+		})
 	})
 
 	t.Run("fund namespace - empty", func(t *testing.T) {
@@ -57,6 +77,48 @@ func Test_Fund(t *testing.T) {
 		cfg := Config{Namespace: "swarm"}
 		err := Fund(ctx, cfg, nl, w)
 		assert.NoError(t, err)
+	})
+
+	t.Run("fund namespace - valid bee node", func(t *testing.T) {
+		t.Parallel()
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			_, err := w.Write([]byte(`
+			{
+				"walletAddress": "0x95f8916183f7C7154e49396507F5b0FafA4d8077",
+				"chainID": 100
+			}
+			`))
+			assert.NoError(t, err)
+		}))
+		t.Cleanup(server.Close)
+
+		t.Run("already funded (0,0)", func(t *testing.T) {
+			t.Parallel()
+
+			nl := fundermock.NewNodeLister([]NodeInfo{{Address: server.URL}})
+			cfg := Config{Namespace: "swarm"}
+			err := Fund(ctx, cfg, nl, w)
+			assert.NoError(t, err)
+		})
+
+		t.Run("already funded (1,1)", func(t *testing.T) {
+			t.Parallel()
+
+			nl := fundermock.NewNodeLister([]NodeInfo{{Address: server.URL}})
+			cfg := Config{Namespace: "swarm", MinAmounts: MinAmounts{NativeCoin: 1, SwarmToken: 1}}
+			err := Fund(ctx, cfg, nl, w)
+			assert.NoError(t, err)
+		})
+
+		t.Run("not funded (3,3)", func(t *testing.T) {
+			t.Parallel()
+
+			nl := fundermock.NewNodeLister([]NodeInfo{{Address: server.URL}})
+			cfg := Config{Namespace: "swarm", MinAmounts: MinAmounts{NativeCoin: 3, SwarmToken: 3}}
+			err := Fund(ctx, cfg, nl, w)
+			assert.NoError(t, err)
+		})
 	})
 }
 
@@ -85,10 +147,19 @@ func Test_CalcTopUpAmount(t *testing.T) {
 
 	for _, tc := range tests {
 		got := CalcTopUpAmount(tc.min, toBigInt(tc.currAmount), tc.tokenDecimals)
-		if got.String() != tc.expected {
-			t.Fatalf("got %s, want %s", got, tc.expected)
-		}
+		assert.Equal(t, tc.expected, got.String())
 	}
+}
+
+func Test_FormatAmount(t *testing.T) {
+	t.Parallel()
+
+	assert.Equal(t, "0", FormatAmount(nil, 0))
+	assert.Equal(t, "0", FormatAmount(nil, 10))
+	assert.Equal(t, "1000", FormatAmount(big.NewInt(1000), 0))
+	assert.Equal(t, "10", FormatAmount(big.NewInt(1000), 2))
+	assert.Equal(t, "10.1", FormatAmount(big.NewInt(1010), 2))
+	assert.Equal(t, "10.01", FormatAmount(big.NewInt(1001), 2))
 }
 
 func toBigInt(val string) *big.Int {
