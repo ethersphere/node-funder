499d32372726df16ab5cedd407b4f6203c2d80f4
Ljubisa <ljubisa.rs@gmail.com>
2023-12-18 09:13:12 +0100

    wip: add beekeeper logger

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,23 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
+	"strings"
 
+	"github.com/ethersphere/beekeeper/pkg/logging"
 	"github.com/ethersphere/node-funder/pkg/funder"
 	"github.com/spf13/cobra"
 )
 
+const (
+	optionLogVerbosity string = "log-verbosity"
+)
+
 func main() {
 	cfg := funder.Config{}
+	var logLevel string
 
 	rootCmd := &cobra.Command{
 		Use: "funder",
@@ -24,11 +33,18 @@ func main() {
 		},
 	}
 
+	rootCmd.PersistentFlags().StringVar(&logLevel, optionLogVerbosity, "info", "log verbosity level 0=silent, 1=error, 2=warn, 3=info, 4=debug, 5=trace")
+
+	logger, err := newLogger(rootCmd, logLevel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fundCmd := &cobra.Command{
 		Use:   "fund",
 		Short: "fund (top up) bee node wallets",
 		Run: func(cmd *cobra.Command, args []string) {
-			doFund(cfg)
+			doFund(cfg, logger)
 		},
 	}
 
@@ -43,52 +59,72 @@ func main() {
 		Use:   "stake",
 		Short: "stake (top up) bee nodes",
 		Run: func(cmd *cobra.Command, args []string) {
-			doStake(cfg)
+			doStake(cfg, logger)
 		},
 	}
 	stakeCmd.PersistentFlags().StringVar(&cfg.Namespace, "namespace", "", "kubernetes namespace")
 	stakeCmd.PersistentFlags().Float64Var(&cfg.MinAmounts.SwarmToken, "minSwarm", 0, "specifies min amount of swarm tokens (BZZ) nodes should have staked")
 
-	rootCmd.AddCommand(fundCmd)
-	rootCmd.AddCommand(stakeCmd)
+	rootCmd.AddCommand(fundCmd, stakeCmd)
 
 	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 }
 
-func doFund(cfg funder.Config) {
+func doFund(cfg funder.Config, logger logging.Logger) {
 	ctx := context.Background()
 
 	if cfg.Namespace == "" && len(cfg.Addresses) == 0 {
-		log.Fatalf("--namespace or --addresses must be set")
+		logger.Fatalf("--namespace or --addresses must be set")
 		return
 	}
 
 	if cfg.ChainNodeEndpoint == "" {
-		log.Fatalf("--chainNodeEndpoint must be set")
+		logger.Fatalf("--chainNodeEndpoint must be set")
 		return
 	}
 
 	if cfg.WalletKey == "" {
-		log.Fatalf("--walletKey must be set")
+		logger.Fatalf("--walletKey must be set")
 		return
 	}
 
 	if err := funder.Fund(ctx, cfg, nil, nil); err != nil {
-		log.Fatalf("error while funding: %v", err)
+		logger.Fatalf("error while funding: %v", err)
 	}
 }
 
-func doStake(cfg funder.Config) {
+func doStake(cfg funder.Config, logger logging.Logger) {
 	ctx := context.Background()
 
 	if cfg.Namespace == "" {
-		log.Fatalf("--namespace must be set")
+		logger.Fatalf("--namespace must be set")
 		return
 	}
 
 	if err := funder.Stake(ctx, cfg, nil); err != nil {
-		log.Fatalf("error while funding: %v", err)
+		logger.Fatalf("error while funding: %v", err)
 	}
+}
+
+func newLogger(cmd *cobra.Command, verbosity string) (logging.Logger, error) {
+	var logger logging.Logger
+	switch strings.ToLower(verbosity) {
+	case "0", "silent":
+		logger = logging.New(io.Discard, 0)
+	case "1", "error":
+		logger = logging.New(cmd.OutOrStdout(), 2)
+	case "2", "warn":
+		logger = logging.New(cmd.OutOrStdout(), 3)
+	case "3", "info":
+		logger = logging.New(cmd.OutOrStdout(), 4)
+	case "4", "debug":
+		logger = logging.New(cmd.OutOrStdout(), 5)
+	case "5", "trace":
+		logger = logging.New(cmd.OutOrStdout(), 6)
+	default:
+		return nil, fmt.Errorf("unknown %s level %q, use help to check flag usage options", optionLogVerbosity, verbosity)
+	}
+	return logger, nil
 }
